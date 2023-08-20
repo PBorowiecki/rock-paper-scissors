f0d46ec8e57260aad12bee718e2c73ed2bf01112
PBorowiecki <borro2603@gmail.com>
2023-08-20 15:28:10 +0200

    Add game functions and run it 5 times

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-function getComputerChoice() {
-  const sign = ["rock", "paper", "scissors"];
-  let index = Math.floor(Math.random() * sign.length);
-  return sign[index];
-}
-
-const playerSelection = prompt(
-  "What is your choice?",
-  "rock, paper, scissors"
-).toLowerCase();
-
-function playRound(playerSelection, computerSelection) {
-  if (
-    playerSelection == "rock" ||
-    playerSelection == "paper" ||
-    playerSelection == "scissors"
-  ) {
-    if (playerSelection == computerSelection) {
-      alert("Draw!");
-    } else if (
-      (playerSelection == "rock" && computerSelection == "scissors") ||
-      (playerSelection == "paper" && computerSelection == "rock") ||
-      (playerSelection == "scissors" && computerSelection == "paper")
-    ) {
-      alert(`You win, ${playerSelection} beat ${computerSelection}!`);
-    } else {
-      alert(`You lose, ${computerSelection} beat ${playerSelection}!`);
-    }
-  } else {
-    alert("Incorrect value. Choose rock, paper or scissors");
+function game() {
+  function getComputerChoice() {
+    const sign = ["rock", "paper", "scissors"];
+    let index = Math.floor(Math.random() * sign.length);
+    return sign[index];
   }
+
+  const playerSelection = prompt(
+    "What is your choice?",
+    "rock, paper, scissors"
+  ).toLowerCase();
+
+  function playRound(playerSelection, computerSelection) {
+    if (
+      playerSelection == "rock" ||
+      playerSelection == "paper" ||
+      playerSelection == "scissors"
+    ) {
+      if (playerSelection == computerSelection) {
+        alert("Draw!");
+      } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+      ) {
+        alert(`You win, ${playerSelection} beat ${computerSelection}!`);
+      } else {
+        alert(`You lose, ${computerSelection} beat ${playerSelection}!`);
+      }
+    } else {
+      alert("Incorrect value. Choose rock, paper or scissors");
+    }
+  }
+
+  const computerSelection = getComputerChoice();
+
+  console.log(playRound(playerSelection, computerSelection));
 }
 
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+for (let i = 0; i < 5; i++) {
+  game(i);
+}
