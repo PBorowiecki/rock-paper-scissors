5965ea31b7071f66fb46e7cda29cb8f17f1c9379
PBorowiecki <borro2603@gmail.com>
2023-08-20 10:48:23 +0200

    Add a playRound comparison function"

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ const playerSelection = prompt(
   "What is your choice?",
   "rock, paper, scissors"
 ).toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    playerSelection == "rock" ||
+    playerSelection == "paper" ||
+    playerSelection == "scissors"
+  ) {
+    if (playerSelection == computerSelection) {
+      alert("Draw!");
+    } else if (
+      (playerSelection == "rock" && computerSelection == "scissors") ||
+      (playerSelection == "paper" && computerSelection == "rock") ||
+      (playerSelection == "scissors" && computerSelection == "paper")
+    ) {
+      alert(`You win, ${playerSelection} beat ${computerSelection}!`);
+    } else {
+      alert(`You lose, ${computerSelection} beat ${playerSelection}!`);
+    }
+  } else {
+    alert("Incorrect value. Choose rock, paper or scissors");
+  }
+}
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
