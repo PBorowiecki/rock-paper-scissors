b2e2f52431e65d4e0935822c5c301e59c3cff647
PBorowiecki <borro2603@gmail.com>
2023-09-12 20:22:21 +0200

    add scoreboard

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btns = [...document.querySelectorAll("button")];
 const showResult = document.querySelector(".show-result");
+const scoreboard = document.querySelector(".scoreboard");
 
 function getComputerChoice() {
   const sign = ["rock", "paper", "scissors"];
@@ -8,6 +9,9 @@ function getComputerChoice() {
 }
 
 let playerSelection = "";
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (
@@ -17,18 +21,22 @@ function playRound(playerSelection, computerSelection) {
   ) {
     if (playerSelection == computerSelection) {
       showResult.textContent = "Draw!";
+      round++;
     } else if (
       (playerSelection == "rock" && computerSelection == "scissors") ||
       (playerSelection == "paper" && computerSelection == "rock") ||
       (playerSelection == "scissors" && computerSelection == "paper")
     ) {
       showResult.textContent = `You win, ${playerSelection} beat ${computerSelection}!`;
+      playerScore++;
     } else {
       showResult.textContent = `You lose, ${computerSelection} beat ${playerSelection}!`;
+      computerScore++;
     }
   } else {
     alert("Incorrect value. Choose rock, paper or scissors");
   }
+  scoreboard.textContent = `Round: ${round} - Player: ${playerScore} - Computer: ${computerScore}`;
 }
 
 const computerSelection = getComputerChoice();
